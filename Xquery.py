43708640a4018c02227d74b826b9b9f19d19f86d
Peter Marek <peter.marek@stfc.ac.uk>
2019-12-06 12:54:24 +0000

    edit try and except blocks

--- Xquery.py
+++ Xquery.py
@@ -10,8 +10,6 @@ def has_coordinates(ds, coords):
     except Exception:
         return False
 
-    print(idx, coords)
-
     if not len(idx) >= len(coords):
         return False
     return all([i == j for i, j in zip(idx, coords)])
@@ -20,45 +18,49 @@ def has_coordinates(ds, coords):
 def has_variables(ds, variables):
     try:
         b = list(ds.data_vars.keys())
-        c = set(variables)
-        return c.issubset(b)
     except Exception:
         return False
+
+    c = set(variables)
+    return c.issubset(b)
 
 
 def has_attribute(ds, variable, value):
     try:
         attr = ds[variable].attrs
-        if attr:
-            result = attr['units']
-            if result == value and isinstance(result, str):
-                    return True
-        return False
     except Exception:
         return False
+
+    result = attr['units']
+    if result == value and isinstance(result, str):
+        return True
+    return False
+
 
 
 def has_shape(ds, variable):
     try:
         result = ds[variable].shape
-        if len(result) == 3:
-            return True
-        return False
     except Exception:
         return False
+
+    if len(result) == 3:
+        return True
+    return False
 
 
 def is_in_range(ds, coord_variable, lower_bound, upper_bound, inclusive=True):
     try:
         lower_bnd = ds.coords[coord_variable].values[0]
         upper_bnd = ds.coords[coord_variable].values[-1]
-        lower = lower_bound
-        upper = upper_bound
-        if lower_bnd <= lower and upper_bnd >= upper:
-            return True
-        return False
     except Exception:
         return False
+
+    lower = lower_bound
+    upper = upper_bound
+    if lower_bnd <= lower and upper_bnd >= upper:
+        return True
+    return False
 
 
 
@@ -79,7 +81,11 @@ def main():
     files = glob.glob(absolute_path + '/tas_Amon*.nc')
 
     opened = open_dataset(files[0])
-    print(has_attribute(opened, 'tas', 'K'))
+    # print(has_attribute(opened, 'tas', 'D'))
+    # print(has_shape(opened, 'tas'))
+    # print(is_in_range(opened, 'lat', -70, 90))
+    # print(has_variables(opened, ['tas', 'tasmax']))
+    print(has_coordinates(opened, ['time', 'lat', 'lon']))
 
 
 if __name__=='__main__':
