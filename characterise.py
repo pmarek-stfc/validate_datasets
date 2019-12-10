40cbf383a0b7fafe67f07d27b1afc759a50c358e
Peter Marek <peter.marek@stfc.ac.uk>
2019-12-10 15:45:39 +0000

    first version of finished functions

--- characterise.py
+++ characterise.py
@@ -2,6 +2,7 @@ import re
 import glob
 import os
 from dataset_utils import *
+import xarray as xr
 
 def open_mfdatasets(files_to_open):
     """
@@ -104,19 +105,52 @@ class Characterise:
         self.checks['coords_check'] = 'WRONG DIMENSIONS', a
         return self.checks
 
-    def check_lat_in_range(self):
-        pass
-    def check_lon_in_range(self):
-        pass
+    def check_lat_in_range(self, lower_bnd, upper_bnd):
+        """
+            Check range of latitude
+        :param lower_bnd: lower bound latitude
+        :param upper_bnd: upper bound latitude
+        :return: 'checks' dictionary with a result
+        """
+        files_found = glob.glob(self.path + '/*.nc')
+        ds = open_mfdatasets(files_found)
+        result = is_in_range(ds, 'lat', lower_bnd, upper_bnd)
+        if result:
+            self.checks['latitude_check'] = 'GOOD'
+            return self.checks
+        self.checks['latitude_check'] = 'INCORRECT RANGE', (lower_bnd, upper_bnd)
+        return self.checks
+
+    def check_lon_in_range(self, lower_bnd, upper_bnd):
+        """
+            Check range of longitude
+        :param lower_bnd: lower bound longitude
+        :param upper_bnd: upper bound longitude
+        :return: 'checks' dictionary with a result
+        """
+        files_found = glob.glob(self.path + '/*.nc')
+        ds = open_mfdatasets(files_found)
+        result = is_in_range(ds, 'lon', lower_bnd, upper_bnd)
+        if result:
+            self.checks['longitude_check'] = 'GOOD'
+            return self.checks
+        self.checks['longitude_check'] = 'INCORRECT RANGE', (lower_bnd, upper_bnd)
+        return self.checks
 
 
 if __name__=='__main__':
-    # test_check_path = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113')
-    # print(test_check_path.check_path())
-    # test_check_varnames = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
-    # print(test_check_varnames.check_varnames())
-    # test_check_var_in_file = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
-    # print(test_check_var_in_file.check_var_in_file())
+    test_check_path = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113')
+    print(test_check_path.check_path())
+    test_check_varnames = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    print(test_check_varnames.check_varnames())
+    test_check_var_in_file = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    print(test_check_var_in_file.check_var_in_file())
     test_check_var_has_coords = Characterise(
         '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
     print(test_check_var_has_coords.check_var_has_coords(["time", "lat", "lon"]))
+    test_check_lat_in_range = Characterise(
+        '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    print(test_check_lat_in_range.check_lat_in_range(-90, 90))
+    test_check_lon_in_range = Characterise(
+        '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    print(test_check_lon_in_range.check_lon_in_range(0, 90))
