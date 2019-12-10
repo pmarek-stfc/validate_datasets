de3f093335624cc605518db721da5645fd5d117c
Peter Marek <peter.marek@stfc.ac.uk>
2019-12-10 15:19:19 +0000

    add functions

--- characterise.py
+++ characterise.py
@@ -3,12 +3,34 @@ import glob
 import os
 from dataset_utils import *
 
+def open_mfdatasets(files_to_open):
+    """
+        :param files_to_open: netCDF4 files
+        :return: opened netCDF datasets using `open_mfdataset`
+    """
+    try:
+        # ALWAYS USE combine='by_coords' with open_mfdataset
+        with xr.open_mfdataset(files_to_open, combine='by_coords') as ds:
+            return ds
+    except Exception as e:
+        return e
+
+def _get_var_id(files_found):
+    """
+    :param files_found: list of paths to NetCDF4 files
+    :return: variable ID of the file
+    """
+    first_file = files_found[0]
+    split_name = first_file.strip('/').split('/')
+    file_name = split_name[-1]
+    var_id = file_name.strip('_').split('_')[0]
+    return var_id
+
 class Characterise:
     def __init__(self, path, project='cmip5', checks={}): #s
         self.path = path
         self.project = project
         self.checks = checks # store results of all checks
-
 
     def check_path(self):
         """
@@ -43,9 +65,9 @@ class Characterise:
         if len(var_names) == 1:
             self.checks['varnames_check'] = 'GOOD'
             return self.checks
-        else:
-            self.checks['INCONSISTENT VARIABLE NAMES'] = var_names
-            return self.checks
+
+        self.checks['INCONSISTENT VARIABLE NAMES'] = var_names
+        return self.checks
 
     def check_var_in_file(self):
         """
@@ -55,30 +77,46 @@ class Characterise:
         """
         files_found = glob.glob(self.path + '/*.nc')
         first_file = files_found[0]
-        split_name = first_file.strip('/').split('/')
-        file_name = split_name[-1]
-        split_file_name = file_name.strip('_').split('_')[0]
+        var_ID = _get_var_id(files_found)
 
         ds = open_file(first_file)
-        print(ds.data_vars.keys())
-        if has_variables(ds, [split_file_name]):
+        if has_variables(ds, [var_ID]):
             self.checks['var_check'] = 'GOOD'
             return self.checks
+
         self.checks['var_check'] = 'NOT PRESENT'
         return self.checks
 
-    def check_var_has_coords(self):
-        pass
+    def check_var_has_coords(self, coords):
+        """
+            Check if the variable ID has coordinates
+        :param coords: list of coords for example ["time", "lat", "lon"]
+        :return: 'checks' dictionary with a result
+        """
+        files_found = glob.glob(self.path + '/*.nc')
+        var_id = _get_var_id(files_found)
+
+        ds = open_mfdatasets(files_found)
+        a = set(list(ds[var_id].indexes.keys()))
+        if a.issubset(coords):
+            self.checks['coords_check'] = 'GOOD'
+            return self.checks
+        self.checks['coords_check'] = 'WRONG DIMENSIONS', a
+        return self.checks
+
     def check_lat_in_range(self):
         pass
     def check_lon_in_range(self):
         pass
-    
+
 
 if __name__=='__main__':
     # test_check_path = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113')
     # print(test_check_path.check_path())
     # test_check_varnames = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
     # print(test_check_varnames.check_varnames())
-    test_check_var_in_file = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
-    print(test_check_var_in_file.check_var_in_file())
+    # test_check_var_in_file = Characterise('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    # print(test_check_var_in_file.check_var_in_file())
+    test_check_var_has_coords = Characterise(
+        '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/seaIce/day/r1i1p1/v20110113/sic')
+    print(test_check_var_has_coords.check_var_has_coords(["time", "lat", "lon"]))
